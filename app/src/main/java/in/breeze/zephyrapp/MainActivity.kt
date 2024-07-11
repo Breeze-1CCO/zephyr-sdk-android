8ac40fdeba3ede2209927cd9d2fc680b00053d5f
Sahil Sinha <sahilsinha.dar@gmail.com>
2024-07-12 01:01:41 +0530

    BZ-27691: feat: add skeleton methods in Zephyr
    - added all skeleton methods in Zephyr Class with specified params
    - added simple implementation to render webview on initiate & process
    - updated sdk manifest to required internet permissions
    - updated app module as a example integration app
    - updated app's MainAcitivity with layout & methods to simulate initiate & process

--- app/src/main/java/in/breeze/zephyrapp/MainActivity.kt
+++ app/src/main/java/in/breeze/zephyrapp/MainActivity.kt
@@ -1,47 +1,109 @@
 package `in`.breeze.zephyrapp
 
 import android.os.Bundle
+import android.webkit.WebView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import `in`.breeze.zephyr.Zephyr
 import `in`.breeze.zephyrapp.ui.theme.ZephyrAppTheme
+import org.json.JSONObject
+import java.util.UUID.randomUUID
 
 class MainActivity : ComponentActivity() {
+
+  private lateinit var zephyr: Zephyr
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    enableEdgeToEdge()
+    WebView.setWebContentsDebuggingEnabled(true)
+
+    val activityContext = this
     setContent {
       ZephyrAppTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-          Greeting(
-            name = "Android",
-            modifier = Modifier.padding(innerPadding)
-          )
+        Column(
+          modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(16.dp),
+          horizontalAlignment = Alignment.CenterHorizontally,
+          verticalArrangement = Arrangement.Center,
+        ) {
+          InitiateView {
+            Toast.makeText(activityContext, "Initiate Triggered", Toast.LENGTH_SHORT).show()
+            zephyr = Zephyr()
+
+            val initiatePayload = createInitiatePayload()
+            val initSDKPayload = createSDKPayload(initiatePayload)
+
+            zephyr.initiate(activityContext, initSDKPayload) { callbackEvent ->
+              run {
+                println("callbackEvent: " + callbackEvent.toString(2))
+              }
+            }
+          }
+
+          Spacer(modifier = Modifier.height(16.dp))
+          ProcessView {
+            Toast.makeText(activityContext, "Process Triggered", Toast.LENGTH_SHORT).show()
+            zephyr.process()
+          }
         }
       }
     }
   }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-  Text(
-    text = "Hello $name!",
-    modifier = modifier
-  )
+fun createInitiatePayload(): JSONObject {
+  val initiatePayload = JSONObject()
+  initiatePayload.put("merchantId", "d2cstorebeta")
+  initiatePayload.put("environment", "sandbox")
+  initiatePayload.put("shopUrl", "https://d2c-store-beta.myshopify.com")
+  return initiatePayload
 }
 
-@Preview(showBackground = true)
+fun createSDKPayload(payload: JSONObject): JSONObject {
+  val sdkPayload = JSONObject()
+  sdkPayload.put("requestId", randomUUID().toString())
+  sdkPayload.put("service", "in.breeze.onecco")
+  sdkPayload.put("payload", payload)
+  return sdkPayload
+}
+
 @Composable
-fun GreetingPreview() {
-  ZephyrAppTheme {
-    Greeting("Android")
+fun InitiateView(onClick: () -> Unit) {
+  Row {
+    Button(onClick = onClick) {
+      Text(text = "Initiate")
+    }
   }
 }
+
+@Composable
+fun ProcessView(onClick: () -> Unit) {
+  Row {
+    Button(onClick = onClick) {
+      Text(text = "Process")
+    }
+  }
+}
+
--- zephyr/src/main/java/in/breeze/zephyr/Zephyr.kt
+++ zephyr/src/main/java/in/breeze/zephyr/Zephyr.kt
@@ -1,13 +1,110 @@
 package `in`.breeze.zephyr
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.widget.FrameLayout
+import androidx.annotation.Keep
+import org.json.JSONException
+import org.json.JSONObject
+
+
+typealias CallbackFn = (event: JSONObject) -> Unit;
 
 class Zephyr {
 
-  fun initiate() {
+  private lateinit var initiatePayload: JSONObject;
+  private lateinit var webView: WebView;
+  private lateinit var context: Activity;
+  private var callbackFn: CallbackFn? = null;
+  private var isInitialized: Boolean = false;
 
+
+  @SuppressLint("SetJavaScriptEnabled", "JavascriptInterface")
+  fun initiate(context: Activity, initiatePayload: JSONObject, callbackFn: CallbackFn) {
+    try {
+      this.context = context;
+      this.initiatePayload = initiatePayload;
+      this.callbackFn = callbackFn;
+      this.webView = WebView(context);
+      this.webView.settings.javaScriptEnabled = true;
+      this.webView.addJavascriptInterface(this, "Zephyr");
+      this.webView.loadUrl(this.getAppUrl());
+      this.isInitialized = true;
+    } catch (e: JSONException) {
+      callbackFn(
+        this.createCallbackEvent(
+          "initiateResult",
+          JSONObject()
+            .put("error", e.message)
+            .put("message", "Bad Initiate Payload")
+        )
+      )
+    }
   }
 
   fun process() {
-
+    this.webView.layoutParams = FrameLayout.LayoutParams(
+      ViewGroup.LayoutParams.MATCH_PARENT,
+      ViewGroup.LayoutParams.MATCH_PARENT
+    )
+    val rootView = this.context.window.decorView.findViewById<ViewGroup>(android.R.id.content)
+    rootView?.addView(webView)
   }
+
+  fun handleBackPress(): Boolean {
+    if (this.webView.canGoBack()) {
+      this.webView.goBack();
+      return false;
+    }
+    return true;
+  }
+
+  fun terminate() {
+    this.webView.destroy();
+  }
+
+  @Keep
+  fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+  }
+
+  @Keep
+  fun onRequestPermissionsResult(
+    requestCode: Int,
+    permissions: Array<String>,
+    grantResults: IntArray
+  ) {
+  }
+
+
+  // Utils
+  // Todo: Move to a better place.
+  private fun getAppUrl(): String {
+    val urlString = if (this.getEnvironment() == "sandbox") {
+      "https://app.beta.breeze.in"
+    } else {
+      "https://app.breeze.in"
+    }
+    val merchantId = this.getInitiatePayload().get("merchantId")
+    val shopUrl = this.getInitiatePayload().get("shopUrl")
+    return "$urlString?merchantId=$merchantId&shopUrl=$shopUrl"
+  }
+
+  private fun getEnvironment(): String {
+    return this.getInitiatePayload().getString("environment")
+  }
+
+  private fun getInitiatePayload(): JSONObject {
+    return this.initiatePayload.getJSONObject("payload")
+  }
+
+  private fun createCallbackEvent(eventName: String, eventData: JSONObject): JSONObject {
+    val event = JSONObject()
+    event.put("eventName", eventName)
+    event.put("eventData", eventData)
+    return event
+  }
+
 }
