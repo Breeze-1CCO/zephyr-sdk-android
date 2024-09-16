baa3ad70e154742b7615551eea05e30b10b801a4
Sahil Sinha <sahilsinha.dar@gmail.com>
2024-09-16 11:18:02 +0530

    BZ-34674: refactor: downgrade build kt version & add examples
    - downgraded build kt versions to 1.8.0
    - added example integration for starting checkout
    - updated blazewebview vars name & removed logs

--- app/src/main/java/in/breeze/blazeapp/MainActivity.kt
+++ app/src/main/java/in/breeze/blazeapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package `in`.breeze.blazeapp
 
 import android.os.Bundle
+import android.os.Handler.Callback
 import android.webkit.WebView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -10,12 +11,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -34,6 +40,9 @@ class MainActivity : ComponentActivity() {
 
     val activityContext = this
     setContent {
+
+      val callbackEventsStr = remember { mutableStateOf("Event \n") }
+
       BlazeAppTheme {
         Column(
           modifier = Modifier
@@ -44,7 +53,6 @@ class MainActivity : ComponentActivity() {
           verticalArrangement = Arrangement.Center,
         ) {
           InitiateView {
-            Toast.makeText(activityContext, "Initiate Triggered", Toast.LENGTH_SHORT).show()
             blaze = Blaze()
 
             val initiatePayload = createInitiatePayload()
@@ -52,20 +60,41 @@ class MainActivity : ComponentActivity() {
 
             blaze.initiate(activityContext, initSDKPayload) { callbackEvent ->
               run {
-                println("callbackEvent: " + callbackEvent.toString(2))
+                callbackEventsStr.value =
+                  callbackEventsStr.value + callbackEvent.toString(2) + "\n ------- \n"
               }
             }
           }
-
           Spacer(modifier = Modifier.height(16.dp))
           ProcessView {
             Toast.makeText(activityContext, "Process Triggered", Toast.LENGTH_SHORT).show()
-            blaze.process(JSONObject())
+            blaze.process(createSDKPayload(createProcessPayload()))
           }
+
+          Column(
+            modifier = Modifier
+              .fillMaxSize()
+              .verticalScroll(rememberScrollState())
+          ) {
+            Text(
+              text = callbackEventsStr.value,
+              modifier = Modifier.padding(16.dp)
+            )
+          }
+
         }
       }
     }
   }
+}
+
+
+fun createSDKPayload(payload: JSONObject): JSONObject {
+  val sdkPayload = JSONObject()
+  sdkPayload.put("requestId", randomUUID().toString())
+  sdkPayload.put("service", "in.breeze.onecco")
+  sdkPayload.put("payload", payload)
+  return sdkPayload
 }
 
 fun createInitiatePayload(): JSONObject {
@@ -76,13 +105,81 @@ fun createInitiatePayload(): JSONObject {
   return initiatePayload
 }
 
-fun createSDKPayload(payload: JSONObject): JSONObject {
-  val sdkPayload = JSONObject()
-  sdkPayload.put("requestId", randomUUID().toString())
-  sdkPayload.put("service", "in.breeze.onecco")
-  sdkPayload.put("payload", payload)
-  return sdkPayload
+fun createProcessPayload(): JSONObject {
+  val cartString = """
+    {
+      "token": "c1-473243a885065ad7c911dc334255d73e",
+      "note": "",
+      "attributes": {
+        "bundleData": "-",
+        "discountName": "-",
+        "variant_ids": "-",
+        "target_variant_ids": "-",
+        "discount": "-"
+      },
+      "original_total_price": 100,
+      "total_price": 100,
+      "total_discount": 0,
+      "total_weight": 0.0,
+      "item_count": 1,
+      "items": [
+        {
+          "id": 45938457084219,
+          "properties": {},
+          "quantity": 1,
+          "variant_id": 45938457084219,
+          "key": "45938457084219:7df34184-c212-44dc-8d8e-8954b890646d",
+          "title": "Sony PS5 Digital Standalone",
+          "price": 100,
+          "original_price": 100,
+          "discounted_price": 100,
+          "line_price": 100,
+          "original_line_price": 100,
+          "total_discount": 0,
+          "discounts": [],
+          "sku": "",
+          "grams": 0,
+          "vendor": "Sony",
+          "taxable": true,
+          "product_id": 8561540628795,
+          "product_has_only_default_variant": true,
+          "gift_card": false,
+          "final_price": 100,
+          "final_line_price": 100,
+          "url": "/products/sony-ps5-digital-standalone?variant=45938457084219",
+          "featured_image": {
+            "aspect_ratio": 1.0,
+            "alt": "Sony PS5 Digital Standalone",
+            "height": 679,
+            "url": "https://cdn.shopify.com/s/files/1/0684/1624/1979/files/51wPWj--fAL._SX679.jpg?v=1690641107",
+            "width": 679
+          },
+          "image": "https://cdn.shopify.com/s/files/1/0684/1624/1979/files/51wPWj--fAL._SX679.jpg?v=1690641107",
+          "handle": "sony-ps5-digital-standalone",
+          "requires_shipping": true,
+          "product_type": "",
+          "product_title": "Sony PS5 Digital Standalone",
+          "product_description": "Maximize your play sessions with near instant load times for installed PS5 games. The custom integration of the PS5 console's systems lets creators pull data from the SSD so quickly that they can design games in ways never before possible. Immerse yourself in worlds with a new level of realism as rays of light are individually simulated, creating true-to-life shadows and reflections in supported PS5 games. Play your favorite PS5 games on your stunning 4K TV. Enjoy smooth and fluid high frame rate gameplay at up to 120fps for compatible games, with support for 120Hz output on 4K displays. With an HDR TV, supported PS5 games display an unbelievably vibrant and lifelike range of colors. PS5 consoles support 8K Output, so you can play games on your 4320p resolution display. Immerse yourself in soundscapes where it feels as if the sound comes from every direction. Through your headphones or TV speakers your surroundings truly come alive with Tempest 3D AudioTech in supported games. Experience haptic feedback via the DualSense wireless controller in select PS5 titles and feel the effects and impact of your in-game actions through dynamic sensory feedback. Get to grips with immersive adaptive triggers, featuring dynamic resistance levels which simulate the physical impact of in-game activities in select PS5 games.",
+          "variant_title": null,
+          "variant_options": ["Default Title"],
+          "options_with_values": [{"name": "Title", "value": "Default Title"}],
+          "line_level_discount_allocations": [],
+          "line_level_total_discount": 0,
+          "has_components": false
+        }
+      ],
+      "requires_shipping": true,
+      "currency": "INR",
+      "items_subtotal_price": 100,
+      "cart_level_discount_applications": []
+    }
+    """.trimIndent()
+
+  val processPayload =
+    JSONObject().putOpt("cart", JSONObject(cartString)).put("action", "startCheckout")
+  return processPayload
 }
+
 
 @Composable
 fun InitiateView(onClick: () -> Unit) {
@@ -91,6 +188,7 @@ fun InitiateView(onClick: () -> Unit) {
       Text(text = "Initiate")
     }
   }
+
 }
 
 @Composable
